12bcfba037167ee16215e650bd2e8fe6db08ee27
Zhu Jiale <Jiale.Zhu@amd.com>
2025-08-14 03:41:12 +0000

    code59-check-schedule

--- examples/online_serving/disaggregated_serving_p2p_moriio_xpyd/1noderun.sh
+++ examples/online_serving/disaggregated_serving_p2p_moriio_xpyd/1noderun.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ex
+export GLOO_SOCKET_IFNAME=ens50f0
+export NCCL_SOCKET_IFNAME=ens50f0
 export CUDA_VISIBLE_DEVICES=3
 export HIP_VISIBLE_DEVICES=3
 export VLLM_USE_V1=1 
@@ -13,9 +15,9 @@ export SAFETENSORS_FAST_GPU=1
     --max_seq_len_to_capture 6144 \
     --max-num-batched-tokens 6144 \
     --host 0.0.0.0 \
-    --port 20005 \
+    --port 40005 \
     --enforce-eager \
     --trust-remote-code \
-    --gpu-memory-utilization 0.7 \
+    --gpu-memory-utilization 0.2 \
     --disable-log-request \
-    --served-model-name deepseek-ai/DeepSeek-R1 
+    --served-model-name deepseek-ai/DeepSeek-R1 --kv-transfer-config '{"kv_connector":"MoRIIOConnector","kv_role":"kv_consumer","kv_port":"25001","kv_connector_extra_config":{"proxy_ip":"10.235.192.56","proxy_port":"30001","http_port":"40005","local_ping_port":"32568","proxy_ping_port":"36367"}}'

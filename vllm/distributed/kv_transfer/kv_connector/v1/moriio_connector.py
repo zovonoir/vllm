9af1fcdf53b33caf9270927a40beeb21b9be8509
Zhu Jiale <Jiale.Zhu@amd.com>
2025-08-14 04:32:22 +0000

    code65-check-schedule

--- vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
@@ -387,7 +387,7 @@ class MoRIIOConnectorWorker:
         self.local_ping_port = int(self.kv_transfer_config.kv_connector_extra_config["local_ping_port"]) # P/D节点上报自身信息时使用的port
         self.proxy_ping_port = int(self.kv_transfer_config.kv_connector_extra_config["proxy_ping_port"]) # P/D节点将自身信息上报至这个port
 
-        self.zmq_address = f"{self.local_ip}:{self.kv_transfer_config.kv_connector_extra_config['http_port']}"
+        self.zmq_address = f"{self.local_ip}:{self.kv_transfer_config.kv_connector_extra_config['kv_port']}"
         self.zmq_context = zmq.Context()
 
         self.mori_engine = None
@@ -395,13 +395,13 @@ class MoRIIOConnectorWorker:
         self._ping_thread = None
         if not self.is_producer:
             self.router_socket = self.zmq_context.socket(zmq.ROUTER)
-            # self.router_socket.bind(f"tcp://{self.zmq_address}")
+            self.router_socket.bind(f"tcp://{self.zmq_address}")
             self.poller = zmq.Poller()
-            # self.poller.register(self.router_socket, zmq.POLLIN)
+            self.poller.register(self.router_socket, zmq.POLLIN)
 
             self.mori_engine = IOEngine("consumer",IOEngineConfig(self.local_ip,self.local_kv_port))
-            # self._handle_request_thread = threading.Thread(target = self.handle_proxy_request,daemon=True)
-            # self._handle_request_thread.start()
+            self._handle_request_thread = threading.Thread(target = self.handle_proxy_request,daemon=True)
+            self._handle_request_thread.start()
         if self._rank == 0 and self.proxy_ip != "":
             self._ping_thread = threading.Thread(target=self._ping,args=(self.zmq_context,),daemon=True)
             self._ping_thread.start() # join?

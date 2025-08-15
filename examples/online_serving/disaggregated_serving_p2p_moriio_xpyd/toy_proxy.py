a243b8345d7fd04e26a04ef172d77ec9783696f9
Zhu Jiale <Jiale.Zhu@amd.com>
2025-08-15 07:45:22 +0000

    code76-modify-proxy

--- examples/online_serving/disaggregated_serving_p2p_moriio_xpyd/toy_proxy.py
+++ examples/online_serving/disaggregated_serving_p2p_moriio_xpyd/toy_proxy.py
@@ -41,11 +41,11 @@ def _listen_for_register(hostname, port):
                 pass
             elif data['type'] == "register" and data['role'] == "P":
                 if data['request_address'] not in prefill_instances:
-                    prefill_instances.append(data['request_address'])
+                    prefill_instances.append("http://" + data['request_address']+"/v1/completions")
 
             elif data["type"] == "register" and data['role'] == "D":
                 if data['request_address'] not in decode_instances:
-                    decode_instances.append(data['request_address'])
+                    decode_instances.append("http://" + data['request_address']+"/v1/completions")
 
             print(f"zovlog:====> recv {data},remote_addr={remote_addr},{prefill_instances = },{decode_instances = }")
 

97c4f0f1eb9cfd888fcc1c2d7e31ae674769734a
Zhu Jiale <Jiale.Zhu@amd.com>
2025-08-19 10:20:40 +0000

    code113-register-kvcache

--- vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
@@ -1260,6 +1260,8 @@ class MoRIIOConnectorWorker:
         for layer_name,local_kv_cache_metadata in self.layer_name_to_local_kv_cache_metadata.items():
             self.kv_caches[layer_name][1] += 1234.567
             # self.nixl_wrapper.set_local_memory_metadata(local_kv_cache_metadata[0])
+            local_metadata = self.nixl_wrapper.register_local_tensor(self.kv_caches[layer_name])
+            local_metadata = MemoryDesc.unpack(local_metadata)
             # self.nixl_wrapper.set_remote_memory_metadata(self.layer_name_to_remote_kv_cache_metadata[layer_name][0])
             # for blkid in remote_block_ids:
                 # offset = blkid * stride[0]
@@ -1267,8 +1269,10 @@ class MoRIIOConnectorWorker:
                 # logger.info(f"zovlog:===========>{layer_name = },{offset = },{transfer_size_byte = }")
                 # self.nixl_wrapper.read_remote_data(transfer_size_byte,offset,offset)
                 # self.nixl_wrapper.read_remote_data(self.kv_caches[layername0].nelement() * self.kv_caches[layername0].element_size(),0,0)
+            self.nixl_wrapper.moriio_engine.read(local_metadata,0,self.layer_name_to_remote_kv_cache_metadata[layer_name][0],0,2*63103* 16* 8* 128*2,self.nixl_wrapper.moriio_engine.allocate_transfer_uid())
         logger.info(f"zovlog:=======> wait for all transfer complete!")
-        # self.nixl_wrapper.waiting_for_transfer_complete()
+        import time
+        time.sleep(5)
         logger.info(f"zovlog:============> all transfer complete!")
         print(f"zovlog:============>tensor:{layername0}:::{self.kv_caches[layername0].sum() = }")
         return

d15ce7c1a0a91474cb7590a0136edb7196b61678
Zhu Jiale <Jiale.Zhu@amd.com>
2025-08-19 15:34:21 +0000

    code115-register-kvcache

--- vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
@@ -1300,8 +1300,9 @@ class MoRIIOConnectorWorker:
             # break
             # self.nixl_wrapper.moriio_engine.read(local_metadata,0,MemoryDesc.unpack(self.layer_name_to_remote_kv_cache_metadata[layer_name][0]),0,2*63103* 16* 8* 128*2/128,self.nixl_wrapper.moriio_engine.allocate_transfer_uid())
         logger.info(f"zovlog:=======> wait for all transfer complete!")
-        import time
-        time.sleep(2)
+        # import time
+        # time.sleep(2)
+        self.nixl_wrapper.waiting_for_transfer_complete()
         for layer_name,local_kv_cache_metadata in self.layer_name_to_local_kv_cache_metadata.items():
             print(f"after load ::::::::::: {layer_name = } , {self.kv_caches[layer_name].sum().item() = },{self.kv_caches[layer_name][0,1,0,0,0:32] = }")
             break

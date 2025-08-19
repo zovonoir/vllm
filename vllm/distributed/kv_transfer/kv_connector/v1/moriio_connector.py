17b1c389c702a9a4560ab3536bafe84692929089
Zhu Jiale <Jiale.Zhu@amd.com>
2025-08-19 10:45:10 +0000

    code114-register-kvcache

--- vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
@@ -1259,16 +1259,16 @@ class MoRIIOConnectorWorker:
         stride = [blknum*blksize*hn*hs,blksize*hs*hn,hs*hn,hs,1]
         for layer_name,local_kv_cache_metadata in self.layer_name_to_local_kv_cache_metadata.items():
             # self.kv_caches[layer_name][1] += 1234.567
-            # self.nixl_wrapper.set_local_memory_metadata(local_kv_cache_metadata[0])
-            local_metadata = self.nixl_wrapper.register_local_tensor(self.kv_caches[layer_name])
-            local_metadata = MemoryDesc.unpack(local_metadata)
-            # self.nixl_wrapper.set_remote_memory_metadata(self.layer_name_to_remote_kv_cache_metadata[layer_name][0])
+            self.nixl_wrapper.set_local_memory_metadata(local_kv_cache_metadata[0])
+            # local_metadata = self.nixl_wrapper.register_local_tensor(self.kv_caches[layer_name])
+            # local_metadata = MemoryDesc.unpack(local_metadata)
+            self.nixl_wrapper.set_remote_memory_metadata(self.layer_name_to_remote_kv_cache_metadata[layer_name][0])
             # for blkid in remote_block_ids:
                 # offset = blkid * stride[0]
                 # transfer_size_byte = blksize * hn * hs * self.kv_element_size
                 # logger.info(f"zovlog:===========>{layer_name = },{offset = },{transfer_size_byte = }")
                 # self.nixl_wrapper.read_remote_data(transfer_size_byte,offset,offset)
-                # self.nixl_wrapper.read_remote_data(self.kv_caches[layername0].nelement() * self.kv_caches[layername0].element_size(),0,0)
+            self.nixl_wrapper.read_remote_data(self.kv_caches[layername0].nelement() * self.kv_caches[layername0].element_size(),0,0)
             # self.nixl_wrapper.moriio_engine.read(local_metadata,0,MemoryDesc.unpack(self.layer_name_to_remote_kv_cache_metadata[layer_name][0]),0,2*63103* 16* 8* 128*2/128,self.nixl_wrapper.moriio_engine.allocate_transfer_uid())
         logger.info(f"zovlog:=======> wait for all transfer complete!")
         import time

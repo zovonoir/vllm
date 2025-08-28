558a3b071b6bdeee94030eccffd42edcbb966363
Zhu Jiale <Jiale.Zhu@amd.com>
2025-08-28 09:28:12 +0000

    comment transfer complete 4

--- vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/moriio_connector.py
@@ -116,9 +116,9 @@ class MoRIIOWrapper():
             self.remote_memory_metadata, remote_offset, 
             transfer_size_byte,
             self.moriio_engine.allocate_transfer_uid())
-        while transfer_status.Code() == StatusCode.INIT:
-            pass
-        # self.transfer_status.append(transfer_status)
+        # while transfer_status.Code() == StatusCode.INIT:
+        #     pass
+        self.transfer_status.append(transfer_status)
 
     def waiting_for_transfer_complete(self):
         while self.transfer_status:
@@ -1337,7 +1337,7 @@ class MoRIIOConnectorWorker:
             #     self.nixl_wrapper.read_remote_data(transfer_size_byte,offset_k,offset_k)
 
         logger.info(f"zovlog:=======> wait for all transfer complete!")
-        # self.nixl_wrapper.waiting_for_transfer_complete()
+        self.nixl_wrapper.waiting_for_transfer_complete()
         # for layer_name,local_kv_cache_metadata in self.layer_name_to_local_kv_cache_metadata.items():
         #     print(f"after load ::::::::::: {layer_name = } , {self.kv_caches[layer_name].sum().item() = },{self.kv_caches[layer_name][0,1,0,0,0:32] = }")
         #     break
